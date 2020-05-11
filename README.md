afe31a9206e811790d07fce3d2a500238f805a92
rfong <rachefo@gmail.com>
2020-05-11 00:36:23 -0700

    init basic layout and content from my article

---
+++ README.md
@@ -0,0 +1,28 @@
+# trauma-resources
+-----
+
+## about
+
+I wrote [an article on PTSD resources](https://medium.com/@rhetoricize/d6fd776339a8). It got too long and I decided it needed a quick-ref website.
+
+## technical reqs
+This probably would have worked fine using Jekyll, but this janky approach was pretty fast and light, so oh well.
+- [x] define FAQ categories with ordered lists of resources
+- [x] define resources which can be included like partials across multiple pages
+- [ ] basic mobile responsive nav -> collapsible topbar
+
+### regrets (future to-fixes?)
+- Multi-paragraph items containing HTML are really messy with this approach. I find it acceptable because I'm mostly just copying text out of the article I wrote, but it might be a pain if I end up primarily editing here.
+
+## TODOS
+
+- [ ] vocab feature
+- [ ] caption datatype
+- [ ] about page + links
+- [ ] literally any styling lol
+
+### quickies
+- [ ] index page & link
+- [ ] add amazon affiliate disclaimer
+- [ ] menu current selection cue
+- [ ] feedback form
---
+++ assets/index.js
@@ -0,0 +1,79 @@
+var app = angular.module('myApp', ['ngSanitize']);
+
+// Change Underscore templating from ERB to Mustache style
+_.templateSettings = {
+  interpolate: /\{\{(.+?)\}\}/g
+};
+
+// Change Angular templating from {{var}} to {[var]} to avoid conflict
+app.config(['$interpolateProvider', '$httpProvider',
+            function($interpolateProvider, $httpProvider) {
+  $interpolateProvider.startSymbol('{[');
+  $interpolateProvider.endSymbol(']}');
+}]);
+
+app.controller('myCtrl', function($scope, $http, $sce, $q) {
+	var $ = angular.element;
+
+	$scope.currPage = null;
+
+	/* Load views */
+
+	$scope.loadPageView = function(pageId) {
+		$scope.currPage = $scope.pages[pageId];
+	};
+
+	$scope.toTrustedHTML = function(html) {
+    return $sce.trustAsHtml(html);
+	}
+
+	/* Load JSON */
+  $scope.loadJsonInScope = function(jsonUrl, scopeVar, callback) {
+		var deferred = $q.defer();
+    $http({
+      method: 'GET',
+      url: jsonUrl,
+    }).then(function successCallback(response) {
+      $scope[scopeVar] = response.data;
+      if (callback && _.isFunction(callback)) { callback(); }
+			deferred.resolve();
+    }, function errorCallback(response) {
+			deferred.reject('Unable to fetch ' + jsonUrl);
+    });
+		return deferred.promise;
+  };
+
+	$scope.getCurrPageId = function() {
+		return _.keys(window.urlparams.getUrlParams())[0];
+	};
+
+	/* Load page data */
+	function loadPageData() {
+		return $scope.loadJsonInScope('assets/groups.json', 'pages', function() {
+			// display order
+			$scope.orderedPageIds = _.pluck($scope.pages.pages, 'id');
+			// hash lookup
+			$scope.pages = _.object(_.map($scope.pages.pages, function(page) {
+				return [page.id, page];
+			}));
+  	}).then(function() {
+	  	return $scope.loadJsonInScope('assets/resources.json', 'resources');
+		});
+	}
+	/* On completion, set up page */
+	loadPageData().then(function() {
+		console.log('Successfully loaded page data');
+		// Load page id corresponding to current url hash, if relevant
+		var urlPageId = $scope.getCurrPageId();
+		if (urlPageId === undefined) {
+			// index. skip
+		} else if (_.contains($scope.orderedPageIds, urlPageId)) {
+			$scope.loadPageView(urlPageId);
+		} else {
+			console.error('page ' + urlPageId + ' does not exist, showing index')
+		}
+	}, function() {
+		alert('Could not load page. Please try refreshing.');
+	});
+
+});
